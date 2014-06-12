2bd6996231e02055c0210d58d4a3b3c9fb59d4e5
Augustus Kwembe <daniel.williams1@ntlworld.com>
2014-06-12 10:16:12 -0400

    added task1

---
+++ task1.py
@@ -0,0 +1,9 @@
+print ("Welcome to the currency converter, please enter your name")
+name = raw_input ("name: ")
+
+print ("Please state which currency you are converting from")
+a = int(input("from: "))
+
+print ("Now please enter the currency you are converting to")
+b = int(input("to: "))
+    
