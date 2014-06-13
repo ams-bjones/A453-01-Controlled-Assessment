86c60081a2d8c8af4e2af8eea17a34cc55e35ac4
Augustus Kwembe <daniel.williams1@ntlworld.com>
2014-06-13 10:42:43 -0400

    Task1update

--- task1.py
+++ task1.py
@@ -1,4 +1,4 @@
-currencies = ["Pounds", "Dollars", "Yen", "Euros"]
+currencies = ["GBP","USD", "EUR", "JPY"]
 #This states the available currency's.
 Pounds = 'Pounds'
 Dollars = 'Dollar'
@@ -15,27 +15,40 @@ print ("Hi there {0}!".format(name))
 #The system says 'Hi there !' In the space between the exclamation mark and the word 'there' is where the value is added. 
 #The system will allow any value to be entered, even numbers.  !' In the space between the exclamation mark and the word 'there'
 
+convertrates1 = 1
+convertrates2 = 1.6
+convertrates3 = 1.2
+convertrates4 = 200
+
 print ("Pound", rates[0])
 print ("Dollars", rates[1])
 print ("Euro", rates[2])
 print ("Yen", rates[3])
-
 choice = input("Please enter your choice: ")
 
-if choice == rates[0]:
-    print ("You have selected Pounds")
+entry=float(input("You have selected {0}. Now please enter a sum of money to convert: ".format(currencies[cfrom])))
+answer= entry/rates[cfrom])*rates[cto]
+print ("{0:.2f})".format(answer))
 
-if choice == rates[1]:
-    print ("You have selected Dollars")
-    
-if choice == rates[2]:
-    print ("You have selected Euros")
-    
-if choice == rates[3]:
-    print ("You have selected Yen")
-    
+# if choice == rates[0]:
+#     print ("You have selected Pounds")
+#     answer = (entry*convertrates1[0])
+#     print(answer)
+
+# if choice == rates[1]:
+#     print ("You have selected Dollars")
+#     answer = (entry*convertrates2[1])
+
+# if choice == rates[2]:
+#     print ("You have selected Euros")
+#     answer = (entry*convertrates3[2])
+
+# if choice == rates[3]:
+#     print ("You have selected Yen")
+#     answer = (entry*convertrates4[3])
+
+
+
 converting = input("Please enter the sum you wish to convert in the currency you chose: ")
 
-print ("Converted answer is,"[0]"Pounds, "[1]"Dollars, "[2]"Euros, "[3]"Yens")
-print ("Thank you for using the money converter :)")
 
