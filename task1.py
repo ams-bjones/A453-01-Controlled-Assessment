31db35b99dfeaa8117536e7959f09f3f83a73cfc
Augustus Kwembe <daniel.williams1@ntlworld.com>
2014-06-13 10:52:00 -0400

    Task1update

--- task1.py
+++ task1.py
@@ -7,27 +7,26 @@ Euros = 'Euros'
 #This sets the variable names for the currency's (Lines 3 to 6)
 rates = [1, 2, 3, 4]
 #This makes a variable called 'rates' and has the currency exchange rates stored in the variable
-print ("Welcome to the Currency Converter, please enter your name")
+print ("Welcome to the Currency Converter")
 #Welcomes the user to the converter and asks for the user to enter their name
-name = raw_input ("name: ")
-#The system allows a value to be entered into the space next to 'name:'
-print ("Hi there {0}!".format(name))
-#The system says 'Hi there !' In the space between the exclamation mark and the word 'there' is where the value is added. 
-#The system will allow any value to be entered, even numbers.  !' In the space between the exclamation mark and the word 'there'
+def ask(direction):
+    print ("Pound", rates[0])
+    print ("Dollars", rates[1])
+    print ("Euro", rates[2])
+    print ("Yen", rates[3])
+    choice = int(input("Please enter your choice: "))
+    return (choice)
 
 convertrates1 = 1
 convertrates2 = 1.6
 convertrates3 = 1.2
 convertrates4 = 200
 
-print ("Pound", rates[0])
-print ("Dollars", rates[1])
-print ("Euro", rates[2])
-print ("Yen", rates[3])
-choice = input("Please enter your choice: ")
+cfrom=ask("From")-1
+cto=ask("to")-1
 
 entry=float(input("You have selected {0}. Now please enter a sum of money to convert: ".format(currencies[cfrom])))
-answer= entry/rates[cfrom])*rates[cto]
+answer= (entry/rates[cfrom])*rates[cto]
 print ("{0:.2f})".format(answer))
 
 # if choice == rates[0]:
