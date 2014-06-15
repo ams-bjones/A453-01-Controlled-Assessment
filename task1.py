49c8e45c7d6b92c33e96056b72040b3bdf9e697b
Bwahhhhhhh <daniel.williams1@ntlworld.com>
2014-06-15 11:22:39 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,53 +1,38 @@
-currencies = ["GBP","USD", "EUR", "JPY"]
-#This states the available currency's.
-Pounds = 'Pounds'
-Dollars = 'Dollar'
-Yens = 'Yen'
-Euros = 'Euros'
-#This sets the variable names for the currency's (Lines 3 to 6)
-rates = [1, 2, 3, 4]
-#This makes a variable called 'rates' and has the currency exchange rates stored in the variable
-print ("Welcome to the Currency Converter")
-#Welcomes the user to the converter and asks for the user to enter their name
-def ask(direction):
-    print ("Pound", rates[0])
-    print ("Dollars", rates[1])
-    print ("Euro", rates[2])
-    print ("Yen", rates[3])
-    choice = int(input("Please enter your choice: "))
-    return (choice)
+###Reference: http://stackoverflow.com/questions/20398017/showing-decimal-places-python-2-7-and-decimal-datatype###
+#setup the decimal data type (including number of decimal places)
+import decimal
 
-convertrates1 = 1
-convertrates2 = 1.6
-convertrates3 = 1.2
-convertrates4 = 200
+print("Welcome to the Currency converter!")
+#Welcomes the user to the currency converter
+currencyAmount = decimal.Decimal(raw_input('Please enter the value of which you wish to convert: '))
+currencyType = int(raw_input('please enter the type (1 = pound, 2 = euro, 3 = dollar, 4 = yen): '))
+#asks for the current value and type from the user
 
-cfrom=ask("From")-1
-cto=ask("to")-1
+#set the exchange rates (based on the GBP)
+euro = decimal.Decimal('1.2')
+dollar = decimal.Decimal('1.6')
+yen = decimal.Decimal('200')
 
-entry=float(input("You have selected {0}. Now please enter a sum of money to convert: ".format(currencies[cfrom])))
-answer= (entry/rates[cfrom])*rates[cto]
-print ("{0:.2f})".format(answer))
+#convert the currency into pound sterling
+if (currencyType == 2):
+    currencyAmount = currencyAmount / euro
+elif (currencyType == 3):
+    currencyAmount = currencyAmount / dollar
+elif (currencyType == 4):
+    currencyAmount = currencyAmount / yen
 
-# if choice == rates[0]:
-#     print ("You have selected Pounds")
-#     answer = (entry*convertrates1[0])
-#     print(answer)
+#ask the user what currency they want it converted into
+currencyConvert = int(raw_input('Please enter the currency you wish to convert to (1 = pound, 2 = euro, 3 = dollar, 4 = yen): '))
 
-# if choice == rates[1]:
-#     print ("You have selected Dollars")
-#     answer = (entry*convertrates2[1])
+#convert the currency into the new format (pound already done in previous steps)
+if (currencyConvert == 2):
+    currencyAmount = currencyAmount * euro
+elif (currencyConvert == 3):
+    currencyAmount = currencyAmount * dollar
+elif (currencyConvert == 4):
+    currencyAmount = currencyAmount * yen
 
-# if choice == rates[2]:
-#     print ("You have selected Euros")
-#     answer = (entry*convertrates3[2])
-
-# if choice == rates[3]:
-#     print ("You have selected Yen")
-#     answer = (entry*convertrates4[3])
-
-
-
-converting = input("Please enter the sum you wish to convert in the currency you chose: ")
-
+#displays the end result to the user
+print 'The result of the Currency Conversion was ',  currencyAmount
+print ("Thank you for using the Currency Converter!")
 
